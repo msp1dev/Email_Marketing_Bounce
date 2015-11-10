5d182ff808a94f043dc5e7d0e83f4138ad48950f
most-wanted <Vinay.Gharat@MSP1NOCLPT005.MSP1NOC.local>
2015-11-10 11:59:45 +0530

    Added README.md

--- __openerp__.py
+++ __openerp__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Bounce',
+    'name': 'Email Marketing Bounce',
     'version': '1.0',
     'category': 'Hidden',
     'description': """
-Opt out Bounced customers from mailing campaigns.
-=================================================
+Opt out Bounced customers from mailing campaigns to avoid unnecessary load while performing Email Campaigns.
+------------------------------------------------------------------------------------------------------------
 
     """,
     'depends': ['mass_mailing'],
