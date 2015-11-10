bd4fc0ed38c157e282cdbc8012797d91ad718ecd
most-wanted <Vinay.Gharat@MSP1NOCLPT005.MSP1NOC.local>
2015-11-10 16:05:25 +0530

    Added summary and logo

--- bounce/__openerp__.py
+++ bounce/__openerp__.py
@@ -3,12 +3,16 @@
 {
     'name': 'Email Marketing Bounce',
     'version': '1.0',
-    'category': 'Hidden',
+    'category': 'Tools',
+    'sequence': 1,
+    'summary': 'Opt out Bounced customers from mailing campaigns.',
     'description': """
 Opt out Bounced customers from mailing campaigns to avoid unnecessary load while performing Email Campaigns.
 ------------------------------------------------------------------------------------------------------------
 
     """,
+    'author': 'MSP1 Managed IT Services Pvt Ltd.',
+	'website': 'http://www.msp1services.com',
     'depends': ['mass_mailing'],
     'data': [
         'bounce_view.xml',
